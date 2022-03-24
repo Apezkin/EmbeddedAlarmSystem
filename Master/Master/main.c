d656e6b01e3ad8a549a6f14b5d129377d7086dc7
Aarne <51073470+Apezkin@users.noreply.github.com>
2022-03-24 11:07:12 +0200

    Init Atmel projects

---
+++ Master/Master/main.c
@@ -0,0 +1,18 @@
+/*
+ * Master.c
+ *
+ * Created: 24.3.2022 10.57.55
+ * Author : Aarne
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Slave/Slave/main.c
@@ -0,0 +1,18 @@
+/*
+ * Slave.c
+ *
+ * Created: 24.3.2022 10.59.19
+ * Author : Aarne
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
