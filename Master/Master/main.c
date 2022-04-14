5b1b3f2b0ca52c6e03b8d9baa8aed72a064bf21d
Aleksi Järventausta <aksu.j@hotmail.com>
2022-04-14 11:14:55 +0300

    Init LCD to Master.

--- Master/Master/main.c
+++ Master/Master/main.c
@@ -1,18 +1,31 @@
-/*
- * Master.c
- *
- * Created: 24.3.2022 10.57.55
- * Author : Aarne
- */ 
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include <stdlib.h>
 
-
-int main(void)
+int
+main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+    lcd_init(LCD_DISP_ON);
+    
+    int16_t test_number = 1023;
+    char test_char_array[16]; 
+    itoa(test_number, test_char_array, 10);
 
+    lcd_clrscr();
+
+    lcd_puts("Hello World!");
+    
+    lcd_gotoxy(0,1);
+    lcd_puts(test_char_array);
+
+
+    while(1)
+    {
+
+    }
+    
+    return 0;
+}
