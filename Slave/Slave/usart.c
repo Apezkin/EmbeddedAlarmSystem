fc1b076aaeaf461c52dabc13830e8e3694f4aff4
Aleksi Järventausta <aleksi.jarventausta@student.lut.fi>
2022-04-21 13:40:47 +0300

    add usart to slave

---
+++ Slave/Slave/usart.c
@@ -0,0 +1,47 @@
+//
+// Created by lut on 21.4.2022.
+//
+
+#include "usart.h"
+ void
+USART_init(uint16_t ubrr) // unsigned int
+{
+    /* Set baud rate in the USART Baud Rate Registers (UBRR) */
+    UBRR0H = (unsigned char) (ubrr >> 8);
+    UBRR0L = (unsigned char) ubrr;
+
+    /* Enable receiver and transmitter on RX0 and TX0 */
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0); //NOTE: the ATmega328p has 1 UART: 0
+    // UCSR0B |= (1 << 4) | (1 << 3);
+
+    /* Set frame format: 8 bit data, 2 stop bit */
+    UCSR0C |= (1 << USBS0) | (3 << UCSZ00);
+    // UCSR0C |= (1 << 3) | (3 << 1);
+
+}
+
+ void
+USART_Transmit(unsigned char data, FILE *stream)
+{
+    /* Wait until the transmit buffer is empty*/
+    while(!(UCSR0A & (1 << UDRE0)))
+    {
+        ;
+    }
+
+    /* Put the data into a buffer, then send/transmit the data */
+    UDR0 = data;
+}
+
+ char
+USART_Receive(FILE *stream)
+{
+    /* Wait until the transmit buffer is empty*/
+    while(!(UCSR0A & (1 << UDRE0)))
+    {
+        ;
+    }
+
+    /* Get the received data from the buffer */
+    return UDR0;
+}
---
+++ Slave/Slave/usart.h
@@ -0,0 +1,17 @@
+//
+// Created by lut on 21.4.2022.
+//
+
+#ifndef SLAVE_USART_H
+#define SLAVE_USART_H
+#include <avr/io.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
+#include <stdio.h>
+
+void USART_init(uint16_t ubrr);
+
+int USART_Transmit(char data, FILE *stream);
+
+int USART_Receive(FILE *stream);
+#endif //SLAVE_USART_H
